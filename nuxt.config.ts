f2f558bf3d4d2eb4a2fdce428df0c9c2c8ec6934
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-07-12 21:17:41 +0300

    add page fade effect nav

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   ssr: true,
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
   modules: ['nuxt-swiper', '@vueform/nuxt', '@pinia/nuxt'],
+
 
 
 
--- pages/products/product/[id].vue
+++ pages/products/product/[id].vue
@@ -422,12 +422,52 @@ onBeforeUnmount(() => {
 
 });
 
- // props
- const props = defineProps({
-//   mainData: Object,
-      // postAllCategory: Object,
-  })
+
+
+//SEO
+useHead({
+    title: object_data_single.value[0].acf.seo_title || object_data_single.value[0].title.rendered,
+    meta: [
+        // Description
+        { name: 'description', content: object_data_single.value[0].acf.seo_description || 'Описание по умолчанию' },
+
+        // Keywords (опционально, не влияет сильно на SEO)
+        { name: 'keywords',  content: object_data_single.value[0].acf.klyuchevaya_fraza || 'test' },
+
+        // OpenGraph
+        { property: 'og:title', content: object_data_single.value[0].acf.seo_title },
+        { property: 'og:description', content: object_data_single.value[0].acf.seo_description },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: `${store.domainUrlCurrent}${route.fullPath}` },
+        { property: 'og:image', content: object_data_single.value?.[0]?.acf?.og_image?.url || 'http://syberia.gearsdpz.beget.tech/wp-content/uploads/2025/07/87baa9efe5d849e4f8da67fe01f9e029.jpg' },
+
+        // Twitter Card (если используешь)
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: object_data_single.value[0].acf.seo_title },
+        { name: 'twitter:description', content: object_data_single.value[0].acf.seo_description },
+        { name: 'twitter:image', content: object_data_single.value?.[0]?.acf?.og_image?.url || 'http://syberia.gearsdpz.beget.tech/wp-content/uploads/2025/07/87baa9efe5d849e4f8da67fe01f9e029.jpg' },
+
+        // Индексация / Деиндексация
+        // Например, noindex для черновика:
+        {
+        name: 'robots',
+        content:
+            object_data_single.value[0].acf.indeksacziya_v_poiskovyh_sistemah === 'index'
+            ? 'index, follow'
+            : 'noindex, nofollow'
+        }
+    ],
+    link: [
+        // Canonical (вручную или динамически)
+        { rel: 'canonical', href: `${store.domainUrlCurrent}/blog/categories/${object_data_single.value[0].acf.canonical || route.params.id}` }
+    ]
+})
+
 </script>
+
+
+
+
 
 <style >
 
