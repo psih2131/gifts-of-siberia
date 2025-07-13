e241533d0529228a9965883c040592cdf740c40f
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-07-13 16:42:27 +0300

    home rest api full

--- pages/index.vue
+++ pages/index.vue
@@ -66,7 +66,7 @@
       </section>
 
 
-      <section class="home-about-sec">
+      <section class="home-about-sec" v-if="frontpage[0]?.acf?.sekcziya_3_missiya">
         <div class="container">
 
           <div class="home-about-sec__title-mob">О компании “Дары Сибири”</div>
@@ -86,15 +86,9 @@
             }" -->
 
 
-              <swiper-slide >
+              <swiper-slide v-for="item in frontpage[0].acf.sekcziya_3_missiya.slajder_izobrazhenij" :key="item">
                   <div class="about-home-slider__element">
-                    <img src="@/assets/images/img/about-img-1.jpg" alt="" class="about-home-slider__img">
-                  </div>
-              </swiper-slide>
-
-              <swiper-slide >
-                  <div class="about-home-slider__element">
-                     <img src="@/assets/images/img/about-img-1.jpg" alt="" class="about-home-slider__img">
+                    <img :src="item.izobrazhenie.url" :alt="item.izobrazhenie.alt" class="about-home-slider__img">
                   </div>
               </swiper-slide>
 
@@ -113,54 +107,25 @@
           </div>
 
           <div class="home-about-sec__text-wrapper">
-            <p class="home-about-sec__text">
-              <b>Дары Сибири</b> — это бренд, который соединяет древние традиции и современные технологии. <br><br>
-
-Мы создаем натуральные продукты на основе уникальных даров сибирской природы, чтобы <b>поддерживать здоровье, укреплять иммунитет и вдохновлять на осознанное потребление</b>.
-            </p>
-            <div class="home-about-sec__mission">
-              Наша миссия — приносить пользу людям, предлагая экологически чистые продукты высочайшего качества.
-            </div>
+            <p class="home-about-sec__text" v-html="frontpage[0].acf.sekcziya_3_missiya.tekst"></p>
+            <div class="home-about-sec__mission" v-html="frontpage[0].acf.sekcziya_3_missiya.tekst_v_ramke"></div>
             <img src="@/assets/images/img/about-home-bg.png" alt="" class="home-about-sec__bg-img">
           </div>
         </div>
       </section>
 
 
-      <section class="home-advantages-sec">
+      <section class="home-advantages-sec" v-if="frontpage[0]?.acf?.sekcziya_4_preimushhestva">
         <div class="container">
-          <h2 class="home-advantages-sec__title">чем уникальна чага</h2>
-          <p class="home-advantages-sec__subtitle"><b>Чага (Inonotus obliquus)</b> – это гриб, который растет на березах в холодных северных регионах. 
-Он давно используется в традиционной медицине благодаря своим многочисленным полезным свойствам</p>
+          <h2 class="home-advantages-sec__title" v-html="frontpage[0].acf.sekcziya_4_preimushhestva.zagolovok"></h2>
+          <p class="home-advantages-sec__subtitle" v-html="frontpage[0].acf.sekcziya_4_preimushhestva.podzagolovok"></p>
           <div class="home-advantages-sec__wrapper">
 
-            <div class="home-advantages-sec__element">
-              <p class="home-advantages-sec__element-title">Антиоксидантное действие:</p>
-              <p class="home-advantages-sec__element-text">Чага богата антиоксидантами, которые помогают нейтрализовать свободные радикалы и <b>защищают клетки от окислительного стресса.</b></p>
+            <div class="home-advantages-sec__element" v-for="item in frontpage[0].acf.sekcziya_4_preimushhestva.preimushhestva" :key="item">
+              <p class="home-advantages-sec__element-title" v-html="item.zagolovok_preimushhestva"></p>
+              <p class="home-advantages-sec__element-text" v-html="item.tekst"></p>
             </div>
 
-            <div class="home-advantages-sec__element">
-              <p class="home-advantages-sec__element-title">Противовоспалительное действие:</p>
-              <p class="home-advantages-sec__element-text">Чага содержит вещества, которые помогают <b>уменьшить воспаление в организме</b>, что может быть полезно при различных хронических заболеваниях.</p>
-            </div>
-
-            <div class="home-advantages-sec__element">
-              <p class="home-advantages-sec__element-title">Регуляция уровня сахара:</p>
-              <p class="home-advantages-sec__element-text">Чага может способствовать <b>нормализации уровня сахара в крови</b> 
-, что полезно для людей 
-с диабетом или предиабетом.</p>
-            </div>
-
-            <div class="home-advantages-sec__element">
-              <p class="home-advantages-sec__element-title">Улучшение пищеварения:</p>
-              <p class="home-advantages-sec__element-text">Чага поддерживает здоровье желудочно-кишечного тракта, помогая в <b>лечении язв и гастритов</b>.</p>
-            </div>
-
-            <div class="home-advantages-sec__element">
-              <p class="home-advantages-sec__element-title">Детоксикация организма:</p>
-              <p class="home-advantages-sec__element-text">Чага способствует <b>выведению токсинов</b> из организма 
-и поддерживает функцию печени.</p>
-            </div>
           </div>
         </div>
 
@@ -168,34 +133,29 @@
       </section>
 
 
-      <section class="banner-video-sec">
+      <section class="banner-video-sec" v-if="frontpage[0]?.acf?.sekcziya_5_video?.url">
         <div class="container">
-          <videoBanner />
+          <videoBanner :videoUrl="frontpage[0].acf.sekcziya_5_video.url" />
         </div>
       </section>
 
 
-      <section class="last-news-sec">
+      <section class="last-news-sec" v-if="popularPostList?.length">
         <div class="container">
           <div class="last-news-sec__header">
             <h2 class="last-news-sec__title">Последние статьи и новости</h2>
-            <a href="" class="last-news-sec__link">
+
+            <NuxtLink class="last-news-sec__link" to="/blog">
               <svg width="64" height="52" viewBox="0 0 64 52" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect x="0.5" y="0.5" width="63" height="51" rx="12.5" stroke="#1B3762"/>
               <path d="M42.7071 26.7071C43.0976 26.3166 43.0976 25.6834 42.7071 25.2929L36.3431 18.9289C35.9526 18.5384 35.3195 18.5384 34.9289 18.9289C34.5384 19.3195 34.5384 19.9526 34.9289 20.3431L40.5858 26L34.9289 31.6569C34.5384 32.0474 34.5384 32.6805 34.9289 33.0711C35.3195 33.4616 35.9526 33.4616 36.3431 33.0711L42.7071 26.7071ZM23 26V27H42V26V25H23V26Z" fill="#1B3762"/>
               </svg>
+            </NuxtLink>
 
-            </a>
           </div>
 
           <div class="last-news-sec__row">
-            <newsCard />
-
-            <newsCard />
-
-            <newsCard />
-
-            <newsCard />
+            <newsCard v-for="item in popularPostList" :key="item" :postData="item" :catList="all_categories_post" />
           </div>
 
           <div class="last-news-sec__slider">
@@ -208,36 +168,13 @@
             }"
             >
 
-            <!-- :pagination="{
-            dynamicBullets: true,
-            clickable: true
-            }" -->
-
-
-              <swiper-slide >
+              <swiper-slide v-for="item in popularPostList" :key="item"  >
                   <div class="news-home-slider__element">
-                    <newsCard />
+                    <newsCard :postData="item" :catList="all_categories_post" />
                   </div>
               </swiper-slide>
 
-              <swiper-slide >
-                  <div class="news-home-slider__element">
-                     <newsCard />
-                  </div>
-              </swiper-slide>
-
-              <swiper-slide >
-                  <div class="news-home-slider__element">
-                     <newsCard />
-                  </div>
-              </swiper-slide>
-
-              <swiper-slide >
-                  <div class="news-home-slider__element">
-                     <newsCard />
-                  </div>
-              </swiper-slide>
-                </swiper-container>
+              </swiper-container>
           </div>
         </div>
       </section>
@@ -270,6 +207,8 @@ import newsCard from '@/components/component__news-card.vue'
 //DATA
 const store = useCounterStore()
 
+const route = useRoute()
+
 const heroBannerSec = ref(null)
 
 const aboutHomeSlider = ref(null)
@@ -278,6 +217,8 @@ const newsHomeSlider = ref(null)
 
 const popularProdList = ref([])
 
+const popularPostList = ref([])
+
 // категории
 const { data: all_categories } = await useFetch(
   `${store.serverUrlDomainRequest}/wp-json/wp/v2/products-section`
@@ -285,7 +226,7 @@ const { data: all_categories } = await useFetch(
 
 const { data: frontpage } = await useFetch(`${store.serverUrlDomainRequest}/wp-json/wp/v2/pages?slug=home`)
 
-// получаем рекомендованные посты
+// получаем рекомендованные товары
 try {
   const mainPost = frontpage.value?.[0]
   const chitatTakzhe = mainPost?.acf['sekcziya_2_-_populyarnye_tovary']
@@ -305,9 +246,41 @@ try {
   console.error('Ошибка при загрузке рекомендованных постов:', error)
 }
 
+
+
+// категории
+const { data: all_categories_post } = await useFetch(
+  `${store.serverUrlDomainRequest}/wp-json/wp/v2/blog-category`
+)
+
+// получаем рекомендованные посты
+try {
+  const mainPost = frontpage.value?.[0]
+  const chitatTakzhe = mainPost?.acf?.sekcziya_6_novosti
+
+  if (Array.isArray(chitatTakzhe) && chitatTakzhe.length) {
+    const slugs = chitatTakzhe.map(obj => obj.post_name)
+
+    const promises = slugs.map(slug =>
+      fetch(`${store.serverUrlDomainRequest}/wp-json/wp/v2/my-blog?slug=${slug}`)
+        .then(res => res.json())
+        .then(data => data?.[0] || null)
+    )
+
+    popularPostList.value = await Promise.all(promises)
+  }
+} catch (error) {
+  console.error('Ошибка при загрузке рекомендованных постов:', error)
+}
+
+console.log('popularPostList', popularPostList)
+
 console.log('recomendPostsList', popularProdList)
 
 console.log('frontpage', frontpage)
+
+
+
 
 //METHODS 
 
@@ -337,13 +310,6 @@ const swiperNews = useSwiper(newsHomeSlider, {
 })
 
 
-
-
-
-
-
-
-
 //HOOKS
 onMounted(() => {
   // Добавляем обработчик события scroll
@@ -356,12 +322,44 @@ onBeforeUnmount(() => {
 });
 
 
+//SEO
+useHead({
+    title: frontpage.value[0].acf.seo_title || frontpage.value[0].title.rendered,
+    meta: [
+        // Description
+        { name: 'description', content: frontpage.value[0].acf.seo_description || 'Описание по умолчанию' },
 
- // props
- const props = defineProps({
-//   mainData: Object,
-      // postAllCategory: Object,
-  })
+        // Keywords (опционально, не влияет сильно на SEO)
+        { name: 'keywords',  content: frontpage.value[0].acf.klyuchevaya_fraza || 'test' },
+
+        // OpenGraph
+        { property: 'og:title', content: frontpage.value[0].acf.seo_title },
+        { property: 'og:description', content: frontpage.value[0].acf.seo_description },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: `${store.domainUrlCurrent}${route.fullPath}` },
+        { property: 'og:image', content: frontpage.value?.[0]?.acf?.og_image?.url || 'http://syberia.gearsdpz.beget.tech/wp-content/uploads/2025/07/87baa9efe5d849e4f8da67fe01f9e029.jpg' },
+
+        // Twitter Card (если используешь)
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: frontpage.value[0].acf.seo_title },
+        { name: 'twitter:description', content: frontpage.value[0].acf.seo_description },
+        { name: 'twitter:image', content: frontpage.value?.[0]?.acf?.og_image?.url || 'http://syberia.gearsdpz.beget.tech/wp-content/uploads/2025/07/87baa9efe5d849e4f8da67fe01f9e029.jpg' },
+
+        // Индексация / Деиндексация
+        // Например, noindex для черновика:
+        {
+        name: 'robots',
+        content:
+            frontpage.value[0].acf.indeksacziya_v_poiskovyh_sistemah === 'index'
+            ? 'index, follow'
+            : 'noindex, nofollow'
+        }
+    ],
+    link: [
+        // Canonical (вручную или динамически)
+        { rel: 'canonical', href: `${store.domainUrlCurrent}/blog/categories/${frontpage.value[0].acf.canonical || route.params.id}` }
+    ]
+})
 </script>
 
 
