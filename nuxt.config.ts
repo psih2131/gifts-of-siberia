537cc00433b3bb302fd81ff4c1853ca70a8d0999
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-07-17 16:35:08 +0300

    sitemap fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,32 @@ export default defineNuxtConfig({
   },
   site: {
     url: 'https://psih2131-gifts-of-siberia-34fa.twc1.net',
-    name: 'Gift of siberia'
+    name: 'Gift of siberia',
+
+    routes: async () => {
+      // Тут вызываем API, чтобы получить все id постов
+      const response = await fetch('https://cy18281-wordpress-gre0n.tw1.ru//wp-json/wp/v2/my-blog')
+      const posts = await response.json()
+
+      // Возвращаем массив URL для sitemap
+      return posts.map(post => `/blog/posts/${post.slug}`)
+    }
+
   },
   ssr: true,
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
+
+  robots: {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://psih2131-gifts-of-siberia-34fa.twc1.net/sitemap.xml'
+  },
   modules: [
     'nuxt-swiper',
     '@vueform/nuxt',
