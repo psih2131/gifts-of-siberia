d105145ddd88366ee025b6c4d5779f4fcbbe37e7
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-07-21 19:48:35 +0300

    change request server url

--- stores/counter.js
+++ stores/counter.js
@@ -5,8 +5,8 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
     const router = useRouter();
 
-    const serverUrlDomainRequest = ref('http://syberia.gearsdpz.beget.tech')
-    // const serverUrlDomainRequest = ref('https://cy18281-wordpress-gre0n.tw1.ru')
+    // const serverUrlDomainRequest = ref('http://syberia.gearsdpz.beget.tech')
+    const serverUrlDomainRequest = ref('https://cy18281-wordpress-gre0n.tw1.ru')
 
 
     const domainUrlCurrent = ref('https://psih2131-gifts-of-siberia-34fa.twc1.net')
