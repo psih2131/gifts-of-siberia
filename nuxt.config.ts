20fbe428be823ff531607aa8118828a904845063
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-08-04 07:03:44 +0300

    sitemap fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
         ]
       },
 
-      productCategories: {
+      postCategories: {
         sources: [
           '/api/__sitemap__/wordpress-posts-categories',
         ]
