fc12dbbadcb257904b3d4a7b9895a5d0bc99ec1f
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-08-07 17:22:10 +0300

    add yandex teg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,8 @@ export default defineNuxtConfig({
         {
           name: 'viewport',
           content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-        }
+        },
+        { name: 'yandex-verification', content: 'e2576f975b9b4417' }
       ]
     }
 
